7095e90747a13efc8afb0a4472f8600af8ea3410
AMELE | 038 <amirabas.merdas@gmail.com>
2026-06-16 12:38:52 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ _ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 LINK_PATTERN = re.compile(
     r"(https?://\S+|t\.me/\S+|telegram\.me/\S+|www\.\S+)", re.IGNORECASE
 )
-BADWORDS = ["مادرتو گاییدم", "بیناموس", "حرومزاده", "مادرجنده", "خارکصه", "مادرخر", "خارتو گاییدم"]
+BADWORDS = ["sapojiaji"]
 
 
 def _convert_font(text, chars):
